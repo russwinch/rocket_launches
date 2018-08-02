053d66419378ad63f6fa15dd0f09fd5ff2c28112
Russ Winch <russell.winch@interoute.com>
2018-08-02 15:02:05 +0100

    Implement templates for html files.  Remove redundant folder check in runserver.

--- runserver.py
+++ runserver.py
@@ -2,10 +2,6 @@ from launches import create_app
 
 app = create_app()
 
-def check_required_folders():
-    folders = ['logs',
-               'launches/static/rocket_images'
-               ]
 
 if __name__ == '__main__':
     app.run(debug=True) #, use_reloader=False)
